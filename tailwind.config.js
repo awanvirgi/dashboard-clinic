7e8072267d5f9ffc23d470725e68877112f3ea3d
awansanria <virgiawan.dev15@gmail.com>
2024-11-24 10:19:11 +0700

    Nambahkan Dashboard

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ["./**/*.{html,js}"],
+    theme: {
+        extend: {
+            colors: {
+                "main": "#F2459F",
+                "second": "#FCDAEC"
+            }
+        },
+    },
+    plugins: [],
+}
+
